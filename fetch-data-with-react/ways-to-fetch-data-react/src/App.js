4ab09165f9e2cffbc4ec79420d71136196021475
Ken Vega <ken.vega.g@gmail.com>
2022-01-02 18:52:30 -0500

    add ways to fetch data in react

---
+++ fetch-data-with-react/ways-to-fetch-data-react/src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>hello</h1>
+      <Routes>
+        <Route path="/" element={<div>hello there</div>} />
+        <Route path="/main" element={<div>main there</div>} />
+      </Routes>
+      <h1>links</h1>
+      <Link to="/">home link</Link>
+      <br />
+      <Link to="/main">main link</Link>
+    </div>
+  );
+}
+
+export default App;
