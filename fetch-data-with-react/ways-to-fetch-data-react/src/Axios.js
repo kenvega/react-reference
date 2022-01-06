ae9373cac40f0027eac59709bcb615b96d8feebf
Ken Vega <ken.vega.g@gmail.com>
2022-01-06 18:40:02 -0500

    start with xios

--- fetch-data-with-react/ways-to-fetch-data-react/src/Axios.js
+++ fetch-data-with-react/ways-to-fetch-data-react/src/Axios.js
@@ -1,4 +1,20 @@
 function Axios() {
+  // TODO: change for axios
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://randomuser.me/api")
+      .then((response) => {
+        setData(response);
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => {
+        console.log("data: ", data);
+      });
+  }, []);
+
   return <div>Axios</div>;
 }
 
