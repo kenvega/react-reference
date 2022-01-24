735878f72b06dec9583d894a37721e16f1339b2a
Ken Vega <ken.vega.g@gmail.com>
2022-01-23 23:30:13 -0500

    add todo

--- fetch-data-with-react/src/fetchingComponents/ReactQuery.js
+++ fetch-data-with-react/src/fetchingComponents/ReactQuery.js
@@ -1,10 +1,12 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import axios from "axios";
 
 import { useQuery } from "react-query";
+// TODO: it seems that it needs a QueryClientProvider
+// source: https://react-query.tanstack.com/reference/QueryClientProvider
 
-import User from "../components/User";
-import Loading from "../components/Loading";
+// import User from "../components/User";
+// import Loading from "../components/Loading";
 
 function ReactQuery() {
   // const [data, setData] = useState(null);
@@ -13,9 +15,14 @@ function ReactQuery() {
   // console.log("useQuery: ", useQuery);
   // useQuery needs a unique key for the data we are fetching
 
-  axios.get("https://pokeapi.co/api/v2/pokemon").then((response) => {
-    console.log(response);
-  });
+  // axios.get("https://pokeapi.co/api/v2/pokemon").then((response) => {
+  //   console.log(response.data);
+  // });
+
+  const queryInfo = useQuery("pokemon", () =>
+    fetch("https://pokeapi.co/api/v2/pokemon").then((res) => res.json())
+  );
+  // console.log("queryInfo: ", queryInfo);
 
   // useEffect(() => {
   //   axios("https://randomuser.me/api") // change to "/apia" to trigger an error
