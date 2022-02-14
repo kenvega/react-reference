34728bde934503f48aa176a6c1f94d1bfcb73fce
Ken Vega <ken.vega.g@gmail.com>
2022-02-13 20:09:14 -0500

    epic react: finish styling lesson

--- epic-react/react-fundamentals/src/exercise/05.js
+++ epic-react/react-fundamentals/src/exercise/05.js
@@ -12,9 +12,51 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+const smallBox = (
+  <div
+    className="box box--small"
+    style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}
+  >
+    small lightblue box
+  </div>
+)
+const mediumBox = (
+  <div
+    className="box box--medium"
+    style={{backgroundColor: 'pink', fontStyle: 'italic'}}
+  >
+    medium pink box
+  </div>
+)
+const largeBox = (
+  <div
+    className="box box--large"
+    style={{backgroundColor: 'orange', fontStyle: 'italic'}}
+  >
+    large orange box
+  </div>
+)
+
+// extra credit: create a Box component
+const Box = ({className, style, children}) => {
+  return (
+    <div className={`box ${className}`} style={{fontStyle: 'italic', ...style}}>
+      {children}
+    </div>
+  )
+}
+
+// extra credit 2: accept size prop
+const Box2 = ({className, style, size = 'small', children}) => {
+  return (
+    <div
+      className={`box box--${size} ${className}`}
+      style={{fontStyle: 'italic', ...style}}
+    >
+      {children}
+    </div>
+  )
+}
 
 function App() {
   return (
@@ -22,6 +64,12 @@ function App() {
       {smallBox}
       {mediumBox}
       {largeBox}
+      <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+        Some text box
+      </Box>
+      <Box2 size="medium" style={{backgroundColor: 'lightblue'}}>
+        Other some text box
+      </Box2>
     </div>
   )
 }
