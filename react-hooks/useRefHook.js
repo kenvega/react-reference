0403eb66ae05719f79e389e4ab2d1263526df429
Ken Vega <ken.vega.g@gmail.com>
2022-02-17 15:21:38 -0500

    update useRef

--- react-hooks/useRefHook.js
+++ react-hooks/useRefHook.js
@@ -1,2 +1,3 @@
 // useRef is like a "box" that can hold a mutable value in the `.current` property
 // passing a ref variable to a ref attribute of an element will make React set the ref variable to the DOM node inside the .current property
+// doesn’t notify you when its content changes (does not trigger a re-render)
