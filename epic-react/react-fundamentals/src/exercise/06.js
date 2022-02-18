6c83e5a4a70e1ea0420cf2e0d348f2f04702622b
Ken Vega <ken.vega.g@gmail.com>
2022-02-18 14:58:16 -0500

    epic react finish extra credit 2 validate inputs

--- epic-react/react-fundamentals/src/exercise/06.js
+++ epic-react/react-fundamentals/src/exercise/06.js
@@ -23,21 +23,50 @@ function UsernameForm({onSubmitUsername}) {
   // )
 
   // useRef
-  const usernameInputRef = React.useRef()
+  // const usernameInputRef = React.useRef()
+  // const handleSubmit = e => {
+  //   e.preventDefault()
+  //   const value = usernameInputRef.current.value
+  //   console.log('value: ', value)
+  //   onSubmitUsername(value)
+  // }
+
+  // return (
+  //   <form onSubmit={handleSubmit}>
+  //     <div>
+  //       <label htmlFor="my-input-text">Username:</label>
+  //       <input ref={usernameInputRef} id="my-input-text" type="text" />
+  //     </div>
+  //     <button type="submit">Submit</button>
+  //   </form>
+  // )
+
+  // extra credit: validate lower case username
+
+  const [error, setError] = React.useState(null)
   const handleSubmit = e => {
     e.preventDefault()
-    const value = usernameInputRef.current.value
+    const value = e.target.elements[0].value
     console.log('value: ', value)
     onSubmitUsername(value)
+  }
+
+  const handleChange = event => {
+    const value = event.target.value
+    const isLowerCase = value.toLowerCase() === value
+    setError(!isLowerCase)
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="my-input-text">Username:</label>
-        <input ref={usernameInputRef} id="my-input-text" type="text" />
+        <input id="my-input-text" type="text" onChange={handleChange} />
+        <div>{error ? 'username should be lowercase' : ''}</div>
       </div>
-      <button type="submit">Submit</button>
+      <button type="submit" disabled={error}>
+        Submit
+      </button>
     </form>
   )
 }
