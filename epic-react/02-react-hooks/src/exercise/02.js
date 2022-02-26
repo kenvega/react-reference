44b2b093e96a8926741c6ba427973b4366d1b3ba
Ken Vega <ken@Kens-MacBook-Pro.local>
2022-02-25 20:53:56 -0500

    epic react finished extra credit 1 exercise 2

--- epic-react/02-react-hooks/src/exercise/02.js
+++ epic-react/02-react-hooks/src/exercise/02.js
@@ -3,10 +3,22 @@
 
 import * as React from 'react'
 
+// extra credit 1: in this example you don't need to read on every render the local storage
+//  only the first time but for the other cases what matters is that writing more than reading local storage
+//  defining a function every render it's cheaper than reading local storage on every render
+
 function Greeting({initialName = ''}) {
-  const [name, setName] = React.useState(
-    window.localStorage.getItem('name') || initialName,
-  )
+  console.log('this prints every time the component renders')
+
+  // extra credit 1: lazy state initializate
+  function getInitialNameValue() {
+    console.log(
+      'this prints every time we set the initial value and read local storage',
+    )
+    return window.localStorage.getItem('name') || initialName
+  }
+
+  const [name, setName] = React.useState(getInitialNameValue)
 
   React.useEffect(() => {
     window.localStorage.setItem('name', name)
