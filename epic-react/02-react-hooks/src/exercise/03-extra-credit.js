00c662a7a4dac681a6975de7ebc7261dbb9b8ca4
Ken Vega <ken.vega.g@gmail.com>
2022-03-14 20:47:48 -0500

    epic react add extra credit: colocating state

--- epic-react/02-react-hooks/src/exercise/03-extra-credit.js
+++ epic-react/02-react-hooks/src/exercise/03-extra-credit.js
@@ -2,13 +2,21 @@
 // http://localhost:3000/isolated/exercise/03.js
 // https://codesandbox.io/s/state-colocation-example-k05qq3?file=/src/App.js:726-999
 
+// colocate state when Display component no longer needs the name state
+
 import * as React from 'react'
 
-function Name({name, onNameChange}) {
+function Name() {
+  const [name, setName] = React.useState('')
+
   return (
     <div>
       <label htmlFor="name">Name: </label>
-      <input id="name" value={name} onChange={onNameChange} />
+      <input
+        id="name"
+        value={name}
+        onChange={event => setName(event.target.value)}
+      />
     </div>
   )
 }
@@ -22,21 +30,20 @@ function FavoriteAnimal({animal, onAnimalChange}) {
   )
 }
 
-function Display({name, animal}) {
-  return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>
+function Display({animal}) {
+  return <div>{`Your favorite animal is: ${animal}!`}</div>
 }
 
 function App() {
-  const [name, setName] = React.useState('')
   const [animal, setAnimal] = React.useState('')
   return (
     <form>
-      <Name name={name} onNameChange={event => setName(event.target.value)} />
+      <Name />
       <FavoriteAnimal
         animal={animal}
         onAnimalChange={event => setAnimal(event.target.value)}
       />
-      <Display name={name} animal={animal} />
+      <Display animal={animal} />
     </form>
   )
 }
