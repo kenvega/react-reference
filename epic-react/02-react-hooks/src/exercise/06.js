b4c8dbcf7f2d5f0b7d283598fc36c027618d0020
Ken Vega <ken.vega.g@gmail.com>
2022-05-19 22:45:15 -0500

    epic react use status to make code clearer

--- epic-react/02-react-hooks/src/exercise/06.js
+++ epic-react/02-react-hooks/src/exercise/06.js
@@ -28,39 +28,50 @@ function App() {
 }
 
 function PokemonInfo({pokemonName}) {
+  const [status, setStatus] = useState('idle')
   const [pokemon, setPokemon] = useState(null)
   const [error, setError] = useState(null)
 
   useEffect(() => {
     if (!pokemonName) return
 
-    setError(null)
-    setPokemon(null)
+    setStatus('pending')
+
     fetchPokemon(pokemonName)
       .then(pokemonData => {
         setPokemon(pokemonData)
+        setStatus('resolved')
       })
-      .catch(error => setError(error))
+      .catch(error => {
+        setError(error)
+        setStatus('rejected')
+      })
   }, [pokemonName])
 
-  if (error) {
+  // render different UI based on the status
+  // makes code more clear and avoid bugs
+  if (status === 'idle') {
+    // start
+    return <div>Submit a pokemon</div>
+  } else if (status === 'pending') {
+    // loading
+    return <PokemonInfoFallback name={pokemonName} />
+  } else if (status === 'rejected') {
+    // error on request
     return (
       <div role="alert">
         There was an error:{' '}
         <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
       </div>
     )
+  } else if (status === 'resolved') {
+    // succesful request
+    return <PokemonDataView pokemon={pokemon} />
   }
 
-  if (!pokemonName) {
-    return <div>Submit a pokemon</div>
-  }
-
-  if (pokemonName && !pokemon) {
-    return <PokemonInfoFallback name={pokemonName} />
-  }
-
-  return <PokemonDataView pokemon={pokemon} />
+  throw new Error(
+    'This status state should be impossible because no other status was defined',
+  )
 }
 
 export default App
